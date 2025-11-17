60fc4600f2b8fe5c90f0a967a92784603f37ead2
Roberto-f55 <robertolzte90@gmail.com>
2025-11-17 14:51:29 +0000

    Movie changed

---
+++ src/main/java/software/ulpgc/kata3/architecture/model/Movie.java
@@ -0,0 +1,4 @@
+package software.ulpgc.kata3.architecture.model;
+
+public record Movie(String tittle, int year, int duration) {
+}
--- src/main/java/software/ulpgc/kata3/model/Movie.java
+++
@@ -1,5 +0,0 @@
-package software.ulpgc.kata3.model;
-
-public record Movie(String tittle, int duration) {
-
-}
