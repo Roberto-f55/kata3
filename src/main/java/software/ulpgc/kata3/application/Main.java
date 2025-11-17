1258f00159143c3ef767452de573fae8250ddfea
Roberto-f55 <robertolzte90@gmail.com>
2025-11-17 14:53:06 +0000

    Main changed

--- src/main/java/software/ulpgc/kata3/application/Main.java
+++ src/main/java/software/ulpgc/kata3/application/Main.java
@@ -1,15 +1,31 @@
 package software.ulpgc.kata3.application;
 
-import software.ulpgc.kata3.model.Movie;
-import software.ulpgc.kata3.io.RemoteMovieLoader;
+import software.ulpgc.kata3.architecture.model.Movie;
+import software.ulpgc.kata3.architecture.io.RemoteMovieLoader;
 
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Movie> movies = new RemoteMovieLoader().LoadAll();
-        System.out.println(movies.size()
-        );
+        List<Movie> movies = new RemoteMovieLoader(Main::fromTsv).LoadAll();
+        Histogram histogram = new HistogramBuilder(movies).build(m -> (m.year() / 10) * 10);
+        System.out.println("Número total de películas: " + movies.size());
+        for(Integer bin: histogram){
+            System.out.println(bin + ":" + histogram.count(bin));
+        }
+    }
+
+    private static Movie fromTsv(String str) {
+        return fromTsv(str.split("\t"));
+    }
+
+    private static Movie fromTsv(String[] split) {
+        return new Movie(split[2], toInt(split[5]), toInt(split[7]));
+    }
+
+    private static int toInt(String s) {
+        if (s.equals("\\N")) return -1;
+        return Integer.parseInt(s);
     }
 }
